a2fa1f96409d6e0107edc435801570eb0953dfda
Yuta OGIHARA <ogi@zsrv.net>
2022-06-02 22:14:07 +0900

    refactoring

--- lib/main.dart
+++ lib/main.dart
@@ -30,8 +30,6 @@ class QuizPage extends StatefulWidget {
 class _QuizPageState extends State<QuizPage> {
   List<Icon> scoreKeeper = [];
 
-  int questionNumber = 0;
-
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -44,7 +42,7 @@ class _QuizPageState extends State<QuizPage> {
             padding: EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                quizBrain.getQuestionText(questionNumber),
+                quizBrain.getQuestionText(),
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 25.0,
@@ -69,7 +67,7 @@ class _QuizPageState extends State<QuizPage> {
                 ),
               ),
               onPressed: () {
-                bool correctAnswer = quizBrain.getCorrectAnswer(questionNumber);
+                bool correctAnswer = quizBrain.getCorrectAnswer();
 
                 setState(() {
                   if (correctAnswer == true) {
@@ -83,6 +81,7 @@ class _QuizPageState extends State<QuizPage> {
                       color: Colors.red,
                     ));
                   }
+                  quizBrain.nextQuestion();
                 });
                 //The user picked true.
               },
@@ -105,7 +104,7 @@ class _QuizPageState extends State<QuizPage> {
               ),
               onPressed: () {
                 //The user picked false.
-                bool correctAnswer = quizBrain.getCorrectAnswer(questionNumber);
+                bool correctAnswer = quizBrain.getCorrectAnswer();
                 setState(() {
                   if (correctAnswer == false) {
                     scoreKeeper.add(Icon(
@@ -118,6 +117,7 @@ class _QuizPageState extends State<QuizPage> {
                       color: Colors.red,
                     ));
                   }
+                  quizBrain.nextQuestion();
                 });
               },
             ),
